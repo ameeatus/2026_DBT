d021e5bf2b0f8ff27dd36155398b6b84aaf5ff55
ameeya <ameeatus@gmail.com>
2025-12-28 20:48:12 -0500

    Changes fct model

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -4,20 +4,19 @@ select * from {{ ref('stg_jaffle_shop_customers')}}
 ),
 
 orders as (
-select * from {{ref('stg_jaffle_shop_orders')}}
+select * from {{ref('fct_orders')}}
 ),
 
 customer_orders as (
 
     select
         customer_id,
-
+        sum(coalesce(amount,0)) as lifetime_value,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
 
-    from orders
-
+    from orders as orders
     group by 1
 
 ),
@@ -31,7 +30,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
 
     from customers
 
